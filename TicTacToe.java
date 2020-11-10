c7db068dcf5f2a014df7f654abab530277df56f7
bharti mali <malibharti5@gmail.com>
2020-11-10 17:20:45 +0530

    ADD : Displaying gameboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,45 +1,45 @@
 import java.util.*;
 public class TicTacToe{
-	static char playerInput='X';
-	static char computerInput='O';
-	public static void main(String[] args){
-		char[][] playBoard = {{' ','|',' ','|',' '},
-				{'-','-','-','-','-'},
-				{' ','|',' ','|',' '},
-				{'-','-','-','-','-'},
-				{' ','|',' ','|',' '}};
-		determineInput();
+        public static void main(String[] args){
+                Scanner scanner=new Scanner(System.in);
+                char playerInput= determineInput(scanner);
+                char computerInput='X';
+                if(playerInput=='X'){
+                        computerInput='O';
+                }
+                char[][] gameBoard=initializationGameBoard();
+                //displayBoard(gameBoard);
 
-	}
-	public static void displayGameBoard(char[][] playBoard){
-		for(char[] row:playBoard){
+        }
+        public static char[][] initializationGameBoard(){
+                char[][] playBoard = {{' ','|',' ','|',' '},
+                                {'-','-','-','-','-'},
+                                {' ','|',' ','|',' '},
+                                {'-','-','-','-','-'},
+                                {' ','|',' ','|',' '}};
+                return playBoard;
+        }
+        public static char determineInput(Scanner scanner){
+                System.out.println("Choose an input letter 1.'O' or 2.'X' : ");
+                while(true){
+                        int input=scanner.nextInt();
+                        switch(input){
+                        case 1:
+                                return 'O';
+                        case 2:
+                                return 'X';
+                        default:
+                                System.out.println("Enter a correct input!");
+                        }
+                }
+
+        }
+        public static void displayBoard(char[][] playBoard){
+                for(char[] row:playBoard){
                        for(char element:row){
                                System.out.print(element);
                        }
                        System.out.println();
-        	}
-
-	}
- 	public static void determineInput(){
-                System.out.println("Choose an input letter 1.'O' or 2.'X' : ");
-                Scanner scanner=new Scanner(System.in);
-		while(true){
-                	int input=scanner.nextInt();
-                	if(input==1){
-				playerInput='O';
-                        	computerInput='X';
-				break;
-                	}else if(input==2){
-				playerInput='X';
-                        	computerInput='O';
-				break;
-                	}
-			else{
-				System.out.println("Enter a correct input!");
-			}
-		}
-         	System.out.println("PlayerInput letter: "+playerInput);
-	 	System.out.println("Computer input letter : "+computerInput);
-
-	}
+                }
+        }
 }
