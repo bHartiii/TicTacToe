2a5d91ec424e39f1370ea7b7c25db17c8af7e9d9
bharti mali <malibharti5@gmail.com>
2020-11-10 16:09:34 +0530

    ADD : Taking input symbols

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,14 @@
+import java.util.*;
 public class TicTacToe{
+	static char playerInput='X';
+	static char computerInput='O';
 	public static void main(String[] args){
 		char[][] playBoard = {{' ','|',' ','|',' '},
 				{'-','-','-','-','-'},
 				{' ','|',' ','|',' '},
 				{'-','-','-','-','-'},
 				{' ','|',' ','|',' '}};
+		determineInput();
 
 	}
 	public static void displayGameBoard(char[][] playBoard){
@@ -14,6 +18,28 @@ public class TicTacToe{
                        }
                        System.out.println();
         	}
+
+	}
+ 	public static void determineInput(){
+                System.out.println("Choose an input letter 1.'O' or 2.'X' : ");
+                Scanner scanner=new Scanner(System.in);
+		while(true){
+                	int input=scanner.nextInt();
+                	if(input==1){
+				playerInput='O';
+                        	computerInput='X';
+				break;
+                	}else if(input==2){
+				playerInput='X';
+                        	computerInput='O';
+				break;
+                	}
+			else{
+				System.out.println("Enter a correct input!");
+			}
+		}
+         	System.out.println("PlayerInput letter: "+playerInput);
+	 	System.out.println("Computer input letter : "+computerInput);
 
 	}
 }
